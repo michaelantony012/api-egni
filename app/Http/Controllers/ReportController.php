9e3e059f0962da9c919ab5e4bdad0a6f8b386991
Andreas Christian <inuyasa00729@gmail.com>
2023-08-22 14:47:15 +0700

    Fix Error ReportDashboard

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -18,23 +18,23 @@ class ReportController extends Controller
         }
         $sqlstockkeluar = 'SELECT product_code, product_name, SUM(trans_qty) AS stock_keluar, trans_date, doctype_id FROM inventory_journal a
                 INNER JOIN products b ON a.`id_product`=b.`id`
-                WHERE trans_date <= ? AND doctype_id IN(2,5) and (a.id_product=? or 0=?)
+                WHERE trans_date = ? AND doctype_id IN(2,5) and (a.id_product=? or 0=?) and trans_loc = ?
                 GROUP BY product_code, trans_date, doctype_id, product_name
                 UNION ALL
                 SELECT product_code, product_name, SUM(trans_qty) AS stock_keluar, trans_date, doctype_id FROM inventory_journal a
                 INNER JOIN products b ON a.`id_product`=b.`id`
-                WHERE trans_date <= ? AND doctype_id=4 and (a.id_product=? or 0=?)
+                WHERE trans_date = ? AND doctype_id=4 and (a.id_product=? or 0=?) and trans_loc = ?
                 GROUP BY product_code, trans_date, doctype_id, product_name
                 HAVING SUM(trans_qty)<0';
-        $stockkeluar = DB::select($sqlstockkeluar,[$request->end_date,$id_product,$id_product,$request->end_date,$id_product,$id_product]);
+        $stockkeluar = DB::select($sqlstockkeluar,[$request->end_date,$id_product,$id_product,$request->id_lokasi,$request->end_date,$id_product,$id_product,$request->id_lokasi]);
 
         $newArray['stocKeluar'] = $stockkeluar;
 
         //Omset
         $sqlomset = 'SELECT SUM(grandtotal) AS omset, date_header FROM sales_invoice_h a
-                WHERE flow_seq IN(110,120) AND date_header <= ?
+                WHERE flow_seq IN(100,120) AND date_header = ? and location_id = ?
                 GROUP BY date_header';
-        $omset = DB::select($sqlomset,[$request->end_date]);
+        $omset = DB::select($sqlomset,[$request->end_date, $request->id_lokasi]);
         
         $newArray['omset'] = $omset;
 
@@ -45,9 +45,9 @@ class ReportController extends Controller
         // ]);
 
         $sqlpot = 'SELECT SUM(disc_value) AS potongan, date_header FROM sales_invoice_h a
-                WHERE flow_seq IN(110,120) AND date_header <= ? 
+                WHERE flow_seq IN(100,120) AND date_header = ? and location_id = ?
                 GROUP BY date_header';
-        $potongan = DB::select($sqlpot,[$request->end_date]);
+        $potongan = DB::select($sqlpot,[$request->end_date, $request->id_lokasi]);
 
         
         
